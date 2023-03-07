2304dcfbf5cd99aac67b0d8c6a72eca0901badb7
dagyeong <ekrud926@naver.com>
2023-03-07 09:45:39 +0900

    background

--- js/background.js
+++ js/background.js
@@ -3,7 +3,16 @@ const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
 
 const body = document.querySelector("body");
 const btnBack = document.querySelector("#btnBack");
+
+let ImageNumber;
+if (localStorage.getItem("randomImg") === null) {
+  ImageNumber = 1;
+} else {
+  ImageNumber = parseInt(localStorage.getItem("randomImg"));
+  body.style.backgroundImage = "url('./images/" + images[ImageNumber] + "')";
+}
 function changeImg() {
-  const randomNum = Math.floor(Math.random() * images.length);
-  body.style.backgroundImage = "url('./images/" + images[randomNum] + "')";
+  ImageNumber = Math.floor(Math.random() * images.length);
+  localStorage.setItem("randomImg", ImageNumber.toString());
+  body.style.backgroundImage = "url('./images/" + images[ImageNumber] + "')";
 }
